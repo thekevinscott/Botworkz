bd1485c15d53ad2d3fa8547a09d12684a98b8196
Kevin Scott <thekevinscott@gmail.com>
2010-05-03 14:13:18 -0500

    Messing with js file

--- public/javascripts/project.js
+++ public/javascripts/project.js
@@ -1,22 +1,30 @@
 (function($){
 
 	$.project = function(){
-		//$('#save').click(this.save);
-/*		
-$(document).keyup(function(event){
-clearInterval(autosaver);
-if (old_form_contents != $("#content").serialize()) // if there is a change
-{
-  autosaver       = setTimeout(   "Save()",5000 );    // wait 2 seconds after typing to save
-}
-});
-		*/
-		var save = function(){
+		
+		var autosaver, old_form_contents;
+		var save = function(e){
+			if (e&&e.preventDefault) { e.preventDefault(); }
 			
 		}
 		
+		$('#save').click(save);
+		
+
+		$(document).keyup(function(){
+			clearInterval($.project.autosaver);
+			var content = $("#content").serialize();
+			if (old_form_contents != content) // if there is a change
+			{
+				old_form_contents = content;
+  				$.project.autosaver = setTimeout($.project.save,1000 );    // wait 2 seconds after typing to save
+			}
+		});
+
+		
 		return {
-			
+			save : save,
+			autosaver: autosaver
 		}
 	}();
 }(jQuery));
