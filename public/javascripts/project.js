e1e092e9d5d0d5a36344810518bcd3a38353e8f8
Kevin Scott <thekevinscott@gmail.com>
2010-05-03 14:06:53 -0500

    Getting the thing to work

---
+++ public/javascripts/project.js
@@ -0,0 +1,23 @@
+(function($){
+
+	$.project = function(){
+		//$('#save').click(this.save);
+/*		
+$(document).keyup(function(event){
+clearInterval(autosaver);
+if (old_form_contents != $("#content").serialize()) // if there is a change
+{
+  autosaver       = setTimeout(   "Save()",5000 );    // wait 2 seconds after typing to save
+}
+});
+		*/
+		var save = function(){
+			
+		}
+		
+		return {
+			
+		}
+	}();
+}(jQuery));
+function trace(msg) { if (console&&console.log){console.log(msg);}}
